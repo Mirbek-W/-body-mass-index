e7e846755d88bae24a06e7ca262443f4837af974
Mirbek-W <81816961+Mirbek-W@users.noreply.github.com>
2022-03-19 19:30:27 +0300

    finish

--- imt.py
+++ imt.py
@@ -1,4 +1,6 @@
+from cProfile import label
 from tkinter import *
+from tkinter import font
 
 root=Tk()
 root.geometry("350x380")
@@ -37,24 +39,73 @@ jkl4.place(x=247,y=104)
 but1= Button (root,background="#EC5B25" ,text= "проверить!")
 but1.place(x=220, y=128)
 
-output1=Label(root,font="14")
+output1=Label(root,font="14",fg="#FF0000")
+output2=Label(root,fg="#EC5B25")
+output_V=Label(root,font="14")
+
 
 def fun1(event):
-    input1=float(iop1.get())
-    input2=float(iop2.get())
+    try:
+        input1=float(iop1.get())
+        input2=float(iop2.get())
+        res=float("{0:.2f}".format(input1/((input2/100)*(input2/100))))
 
-    res=float("{0:.2f}".format(input1/((input2/100)*(input2/100))))
+
+    
 
 
    
     
-    if res<=16:
-        output1["text"]="Выраженный дефицит массы тела"
-        print("все работает!")
-    else:
-        print("что то нито")
+        if res<=16:
+            output1["text"]="   Выраженный дефицит массы тела"
+            output2["text"]="ИМТ ниже 18,5 кг/м2 определяется как недостаточная\n масса (16–18,5 кг/м2), ниже 16 кг/м2 — выраженный\n дефицит массы тела"
+            output_V["text"]=res 
+        elif res<=18.5:
+            output1["text"]="Недостаточная масса тела (дефицит)"
+            output2["text"]="В соответствии с классификацией ВОЗ нормальным (здоровым)\nпоказателем ИМТ считается диапазон 18,5–24,99 кг/м2.\nИМТ ниже 18,5 кг/м2 определяется как недостаточная масса\n тела (16–18,5 кг/м2), ниже 16 кг/м2 — выраженный дефицит\n массы тела."
+            output_V["text"]=res
 
+        elif res<=24:
+            output1["text"]="Нормальная масса тела"
+            output2["text"]="Для взрослых нормальной массой тела считается\nтакая, при которой ИМТ находится в интервале\n от 18,5 до 25."
+            output_V["text"]=res
+
+        elif res<=30:
+            output1["text"]="Избыточная масса тела (предожирение)"
+            output2["text"]="сравнить с нормативными показателями – если\n получится небольше 25 единиц – то масса тела не\n превышает норму,при ожирении ИМТ больше 30\n единиц, а состояние с ИМТ в диапазоне\n25-30 называется избыточным весом (предожирением)"
+            output_V["text"]=res
+
+        elif res<=35:
+            output1["text"]="Ожирение I степени"
+            output2["text"]="При первой степени ожирения избыточная \nтела превышает идеальную, или нормальную на 10 – 30%.\nПри 2 степени ожирения – на 30 – 50%. 3 степень\n ожирения – это превышения массы тела на 50 – 100%."
+            output_V["text"]=res
+
+        elif res<=40:
+            output1["text"]="Ожирение II степени"
+            output2["text"]="Ожирение 2 степени — это серьезная патология,\nкоторая несет серьезные эстетические\n изменения тела"
+            output_V["text"]=res
+
+        elif res>40:
+            output1["text"]="Ожирение III степени"
+            output2["text"]="Ожирение третьей степени — значительное повышение\nмассы тела, при котором индекс массы тела (ИМТ)\nсоставляет более 40. Как правило, накопление такой массы\nидет постепенно, т. е. это результат прогрессирующего\n ожирения предыдущих степеней."
+            output_V["text"]=res
+
+        else:
+            output1["text"]=" "
+            output2["text"]=" "
+
+    except ValueError:
+        output1["text"]="не верное значение !"  
+        output2["text"]=" "   
+        output_V["text"]=" " 
+    except UnboundLocalError:
+        output1["text"]="не верное значение !"  
+        output2["text"]=" "   
+        output_V["text"]=" " 
+    
+    output_V.place(x=40,y=180)
     output1.place(x=3,y=200)
+    output2.place(x=0,y=220)
 
  
 
@@ -71,8 +122,6 @@ but1.bind("<Button-1>",fun1)
 
 
 
-
-#output
 
 
 
